e1b8a85e60870f9eacf1d9c82ae933dcb3901139
suuniqo <suuniqo@gmail.com>
2025-11-18 22:21:13 +0100

    quick reformat

--- lib/scheme/color_theme.dart
+++ lib/scheme/color_theme.dart
@@ -5,31 +5,31 @@ class ColorTheme {
 
     static ThemeData get light {
       final baseScheme = ColorScheme.fromSeed(
-          seedColor: seed,
-          brightness: Brightness.light,
-          );
+        seedColor: seed,
+        brightness: Brightness.light,
+      );
 
       return ThemeData(
-          useMaterial3: true,
-          colorScheme: baseScheme.copyWith(
-            outline: baseScheme.primary,
-            ),
-          );
+        useMaterial3: true,
+        colorScheme: baseScheme.copyWith(
+          outline: baseScheme.primary,
+        ),
+      );
     }
 
     static ThemeData get dark {
       final baseScheme = ColorScheme.fromSeed(
-          seedColor: seed,
-          brightness: Brightness.dark,
-          );
+        seedColor: seed,
+        brightness: Brightness.dark,
+      );
 
       return ThemeData(
-          useMaterial3: true,
-          colorScheme: baseScheme.copyWith(
-            primaryContainer: Colors.black87,
-            outline: baseScheme.primary,
-            ),
-          );
+        useMaterial3: true,
+        colorScheme: baseScheme.copyWith(
+          primaryContainer: Colors.black87,
+          outline: baseScheme.primary,
+        ),
+      )r
     }
 
     static ThemeData get base => light;
